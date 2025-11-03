194aece80245f466490310258db3c021ffcda70f
freekhxspoor <freekhexspoor@outlook.com>
2025-11-03 13:33:26 +0100

    Format code

--- src/functions.py
+++ src/functions.py
@@ -1,8 +1,10 @@
-def sum(a,b):
-    return(a+b)
+def sum(a, b):
+    return a + b
 
-def divide(a,b):
-    return(a/b)
 
-def subtract(a,b):
-    return(a-b)
+def divide(a, b):
+    return a / b
+
+
+def subtract(a, b):
+    return a - b
